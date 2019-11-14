79ed387636687df8aa77fe0a4a44565bb7c7f85f
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-11-14 06:38:06 -0800

    use util functions

--- subscription_v3.py
+++ subscription_v3.py
@@ -11,6 +11,7 @@ from db import UPDATE_TIME
 import threading
 import traceback as tb
 import hashlib
+from telegram_util import isMeaningful, splitCommand, log_on_fail, autoDestroy
 
 START_MESSAGE = ('''
 Channel Subscription for channels and groups. Bot needs to be in the subscribed channel.
@@ -33,34 +34,13 @@ with open('config') as f:
 test_channel = -1001459876114
 debug_group = CREDENTIALS.get('debug_group') or -1001198682178
 m_interval = config['message_interval_min']
-
-def isMeaningful(m):
-    if m.media_group_id:
-        return False
-    if m.photo:
-        return True
-    if not m.text:
-        return False
-    if m.text.startswith('/'):
-        return False
-    if len(m.text) < 10:
-        return False
-    return True
-
-def tryDelete(msg):
-    try:
-        msg.delete()
-    except:
-        pass
+updater = Updater(CREDENTIALS['bot_token'], use_context=True)
 
 def tryDeleteById(chat_id, msg_id):
     try:
         updater.bot.delete_message(chat_id = chat_id, message_id = msg_id)
     except:
         pass
-
-def autoDestroy(msg):
-    threading.Timer(60, lambda: tryDelete(msg)).start() 
 
 def getChat(raw, msg):
     raw = str(raw).strip().split('/')[-1]
@@ -78,80 +58,62 @@ def getChat(raw, msg):
         autoDestroy(r)
         return None
 
-def splitCommand(text):
-    pieces = text.split()
-    if len(pieces) < 1:
-        return '', ''
-    command = pieces[0]
-    return command.lower(), text[text.find(command) + len(command):].strip()
-
 def formatSubscription(s):
     return '[' + s['title'] + '](t.me/' + str(s.get('username')) + ')'
 
+@log_on_fail(updater)
 def command(update, context):
-    try:
-        msg = update.effective_message
-        autoDestroy(msg)
-        command, text = splitCommand(msg.text)
-        if 's3_l' in command:
-            subscriptions = dbs.getList(msg.chat_id)
-            subscriptions = [str(index) + ': ' + formatSubscription(x) for \
-                index, x in enumerate(subscriptions)]
-            r = msg.reply_text(
-                'subscription list: \n\n' + '\n'.join(subscriptions), 
-                quote=False,
-                parse_mode='Markdown', 
-                disable_web_page_preview=True)
+    msg = update.effective_message
+    autoDestroy(msg)
+    command, text = splitCommand(msg.text)
+    if 's3_l' in command:
+        subscriptions = dbs.getList(msg.chat_id)
+        subscriptions = [str(index) + ': ' + formatSubscription(x) for \
+            index, x in enumerate(subscriptions)]
+        r = msg.reply_text(
+            'subscription list: \n\n' + '\n'.join(subscriptions), 
+            quote=False,
+            parse_mode='Markdown', 
+            disable_web_page_preview=True)
+        autoDestroy(r)
+        return
+    if 's3_un' in command:
+        try:
+            index = int(text)
+        except:
+            r = msg.reply_text('please give index')
             autoDestroy(r)
             return
-        if 's3_un' in command:
-            try:
-                index = int(text)
-            except:
-                r = msg.reply_text('please give index')
-                autoDestroy(r)
-                return
-            r = dbs.deleteIndex(msg.chat_id, index)
-            autoDestroy(msg.reply_text(r, quote = False))
+        r = dbs.deleteIndex(msg.chat_id, index)
+        autoDestroy(msg.reply_text(r, quote = False))
+        return
+    if 's3_s' in command:
+        chat = getChat(text, msg)
+        if not chat:
             return
-        if 's3_s' in command:
-            chat = getChat(text, msg)
-            if not chat:
-                return
-            r = dbs.add(msg.chat_id, chat.to_dict())
-            autoDestroy(msg.reply_text(r, quote=False))
-            return
-    except Exception as e:
-        updater.bot.send_message(chat_id=debug_group, text=str(e)) 
-        print(e)
-        tb.print_exc()
+        r = dbs.add(msg.chat_id, chat.to_dict())
+        autoDestroy(msg.reply_text(r, quote=False))
+        return
 
+@log_on_fail(updater)
 def manage(update, context):
     global queue
-    try:
-        msg = update.effective_message
-        if (not msg) or (not isMeaningful(msg)):
-            return 
-        chat_id = msg.chat_id
-        if chat_id and chat_id < 0 and dbs.getList(chat_id):
-            dbu.setTime(chat_id)
-        for subscriber in dbs.getSubsribers(chat_id)[::-1]:
-            queue.append((subscriber, msg.chat_id, msg.message_id))
-    except Exception as e:
-        updater.bot.send_message(chat_id=debug_group, text=str(e)) 
-        print(e)
-        tb.print_exc()   
+    msg = update.effective_message
+    if (not msg) or (not isMeaningful(msg)):
+        return 
+    chat_id = msg.chat_id
+    if chat_id and chat_id < 0 and dbs.getList(chat_id):
+        dbu.setTime(chat_id)
+    for subscriber in dbs.getSubsribers(chat_id)[::-1]:
+        queue.append((subscriber, msg.chat_id, msg.message_id)) 
 
 def start(update, context):
     if update.message:
-        update.message.reply_text(START_MESSAGE, quote=False)
+        update.message.reply_text(START_MESSAGE)
 
-updater = Updater(CREDENTIALS['bot_token'], use_context=True)
-dp = updater.dispatcher
-
-dp.add_handler(MessageHandler((~Filters.private) and (Filters.command), command))
-dp.add_handler(MessageHandler((~Filters.private) and (~Filters.command), manage))
-dp.add_handler(MessageHandler(Filters.private, start))
+updater.dispatcher.add_handler(MessageHandler((~Filters.private) and (Filters.command), command))
+updater.dispatcher.add_handler(MessageHandler((~Filters.private) and (~Filters.command), manage))
+updater.dispatcher.add_handler(MessageHandler(Filters.private, start))
 
 def isReady(subscriber):
     return dbu.get(subscriber) + m_interval * SEC_PER_MIN < time.time()
@@ -165,6 +127,7 @@ def findDup(msg):
         return hashes[h]
     hashes[h] = msg.message_id
 
+@log_on_fail(updater)
 def loopImp():
     global queue
     queue_to_push_back = []
@@ -193,19 +156,11 @@ def loopImp():
                 print(item)
                 updater.bot.send_message(chat_id=debug_group, text=str(e))
                 queue_to_push_back.append(item)
-    for item in queue_to_push_back:
+    for item in queue_to_push_back[::-1]:
         queue.append(item)
 
 def loop():
-    try:
-        loopImp()
-    except Exception as e:
-        print(e)
-        tb.print_exc()
-        try:
-            updater.bot.send_message(chat_id=debug_group, text=str(e))
-        except:
-            pass
+    loopImp()
     threading.Timer(m_interval * SEC_PER_MIN, loop).start() 
 
 threading.Timer(1, loop).start()
